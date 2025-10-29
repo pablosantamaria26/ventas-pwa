a86caac0c6dc9bafde8cc5cb557f287b6ca9f844
pablosantamaria26 <santamariapablodaniel@gmail.com>
2025-10-29 10:43:12 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'ventas-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './assets/styles.css',
+  './assets/app.js',
+  './assets/api.js',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE && caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  if(e.request.method==='GET'){
+    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+  }
+});
